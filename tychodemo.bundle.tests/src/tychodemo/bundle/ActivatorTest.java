1e5c70412aaa35ed325cdfd752fa80b575aaab4d
Vikram.Kamath <Vikram.Kamath@noaa.gov>
2013-07-01 17:01:29 -0400

    added bundle test fragments

---
+++ tychodemo.bundle.tests/src/tychodemo/bundle/ActivatorTest.java
@@ -0,0 +1,24 @@
+package tychodemo.bundle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActivatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	//@Test
+	public void testMustFail() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMustPass(){
+		assertTrue(true);
+	}
+
+}
